f1ac7330cd0fb64a498f7dc46e5d793082956baa
Benjamin <benjamincook1313@gmail.com>
2022-04-11 20:45:32 -0600

    tail recursion and memoization

--- fib.js
+++ fib.js
@@ -1,18 +1,24 @@
 
-// const fib = (n) => {
-//   const arr = [1, 1]
-//   for(let i=0; i<=n; i++){
-//     arr.push(arr[i] + arr[i + 1])
-//   }
-//   return arr.pop()
-// };
 
-// RECURSIVE O( 2^n )
+// Exponential Time O(2^n) very slow 
+// Space O(n)
+
 const fib = (n) => {
   if(n <= 2) return 1
   return fib(n - 1) + fib(n - 2)
 };
 
+// TAIL RECURSION fibonacci O(1) *** Super Fast ***
+const fibon = (n) => { 
+  let go = (n, a, b) => {
+    if(n === 0) return a
+    return go(n - 1, b, a+b)
+  };
+
+  return go(n, 0, 1)
+};
 
 
-console.log(fib(7))
+
+
+console.log(fibon(6))
